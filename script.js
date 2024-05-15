c98c0a978ef216b6ffeeb15f3f4aa5012903d233
Fuzer <fuuzer_@hotmail.com>
2024-05-15 15:38:56 -0300

    slider added

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
 const buttons = document.querySelectorAll('.carrossel button');
+const texts = document.getElementById('informations-text')
+const slider = document.querySelector('.slider');
 
 buttons.forEach(btn => {
   btn.addEventListener('click', () => {
     removeClass();
     btn.classList.add('active__btn');
-    console.log(btn)
+    switch (btn) {
+      case buttons[0]:
+        slider.style.setProperty('--selected-item', '0');
+        break;
+      case buttons[1]:
+        slider.style.setProperty('--selected-item', '1');
+        texts.innerHTML = 
+        `
+        <p class="informations__game">Grand Theft Auto Online</p>
+        <h1 class="informations__title">The Cluckin' Bell Farm Raid</h1>
+        <div class="main">
+          <button class="main__btn">Watch now</button>
+        </div>
+        `
+        break;
+      case buttons[2]:
+         slider.style.setProperty('--selected-item', '2');
+         texts.innerHTML = 
+         `
+         <p class="informations__game">Grand Theft Auto Online</p>
+         <h1 class="informations__title">The Chop Shop</h1>
+         <div class="main">
+           <button class="main__btn">Watch now</button>
+         </div>
+         `
+          break;
+        case buttons[3]:
+          slider.style.setProperty('--selected-item', '3');
+          texts.innerHTML = 
+          `
+          <p class="informations__game">Grand Theft Auto Online</p>
+          <h1 class="informations__title">Speed to Victory in New Drag Races</h1>
+          <div class="main">
+            <button class="main__btn">Watch now</button>
+          </div>
+          `
+          break;
+    }
   })
 });
 
